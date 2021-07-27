f50bde7b4398b415d8446918b6942a179a41ee86
Andrew Hawkins <andrewhaekins@gmail.com>
2021-07-27 00:24:30 -0400

    Add datatable

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,33 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import ReactDataGrid from '@inovua/reactdatagrid-community';
+import '@inovua/reactdatagrid-community/index.css';
+
+const columns = [
+  { name: 'name', header: 'Name', minWidth: 50, defaultFlex: 2 },
+  { name: 'age', header: 'Age', maxWidth: 1000, defaultFlex: 1 }
+];
+
+const gridStyle = { minHeight: 550, width: 400 };
+
+const dataSource = [
+  { id: 1, name: 'John McQueen', age: 35 },
+  { id: 2, name: 'Mary Stones', age: 25 },
+  { id: 3, name: 'Robert Fil', age: 27 },
+  { id: 4, name: 'Roger Robson', age: 81 },
+  { id: 5, name: 'Billary Konwik', age: 18 },
+  { id: 6, name: 'Bob Martin', age: 18 },
+  { id: 7, name: 'Matthew Richardson', age: 54 },
+  { id: 8, name: 'Ritchie Peterson', age: 54 },
+  { id: 9, name: 'Bryan Martin', age: 40 },
+  { id: 10, name: 'Mark Martin', age: 44 },
+  { id: 11, name: 'Michelle Sebastian', age: 24 },
+  { id: 12, name: 'Michelle Sullivan', age: 61 },
+  { id: 13, name: 'Jordan Bike', age: 16 },
+  { id: 14, name: 'Nelson Ford', age: 34 },
+  { id: 15, name: 'Tim Cheap', age: 3 },
+  { id: 16, name: 'Robert Carlson', age: 31 },
+  { id: 17, name: 'Johny Perterson', age: 40 }
+];
 
 export default function Home() {
   return (
@@ -12,6 +41,13 @@ export default function Home() {
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <ReactDataGrid
+          idProperty="id"
+          columns={columns}
+          dataSource={dataSource}
+          style={gridStyle}
+        />
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
@@ -54,8 +90,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -205,5 +240,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
